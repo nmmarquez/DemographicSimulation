e3c188a5035ceee4b81fa1e60d4d4ce9de14fcc7
Neal Marquez <nmmarquez@protonmail.com>
2018-02-20 17:53:12 -0800

    transform to r package

--- R/GenPopFuncs.R
+++ R/GenPopFuncs.R
@@ -1,3 +1,82 @@
+#' Generate Mortality Functions For a Location Year
+#'
+#' @description Generates Survial, Hazard, Failure(CDF), 
+#' Inverse Failure(invCDF), derivative of failure, and simulation functions for
+#' a specified country, year, sex.
+#' 
+#' @param location_ character, country name
+#' @param year_  1970 <= int <= 2016, year of mortality function to generate 
+#' @param sex_ character, either 'Female', 'Male', or 'Both'
+#' @param max_age float, float for the max age allowed for simulatuins generally a value >= 120 
+#'
+#' @return List of Demographic Mortality and Simulation Functions
+#' 
+#' Generates Typical Demographic Mortality Functions Based on Global Burden of 
+#' Disease Estimate Data for Survival, Failure(CDF), Hazard, Inverse Failure, 
+#' and the Age of Death Distribution(Derivative of Failure) using a non 
+#' Parametric Spline interpolation of the the (CDF) with strictly >= 0 for
+#' derivatives. Also inlcudes a convience function to simulate data. A list of 
+#' names of possible countries to use for the simulation are shown in the code
+#' example.
+#'
+#' @examples
+#' # Show list of countries we can simulate from 
+#' unique(DFDeath$location)
+#' 
+#' # run plotting and simulation code
+#' require(dplyr)
+#' require(ggplot2)
+#' # Generate the demographic functions for Mexico 2016 Females
+#' MX2016F <- GenPopFuncs(location_="Mexico", year_=1980, sex_="Both")
+#' 
+#' summary(MX2016F$CDF(seq(.010, 100.005, .005)) - MX2016F$CDF(seq(.005, 100, .005)))
+#' summary(MX2016F$CDF(seq(.010, 100.005, .005), deriv=1))
+#' 
+#' # plot some of the functions
+#' data.frame(Age=seq(.01, 120, .01)) %>%
+#'     mutate(CDF=MX2016F$CDF(Age)) %>%
+#'     ggplot(aes(x=Age, y=CDF)) + 
+#'     geom_line() + 
+#'     coord_trans(y="log")
+#' 
+#' data.frame(Age=seq(.1, 100, .005)) %>%
+#'     mutate(Hazard=MX2016F$Hxfunc(Age)) %>%
+#'     ggplot(aes(x=Age, y=Hazard)) + 
+#'     geom_line() + 
+#'     coord_trans(y="log")
+#' 
+#' m <- 10000
+#' system.time(simDeaths <- lapply(1:100, function(y) MX2016F$simPop(m, 1)))
+#' 
+#' MXDeath <- DFDeath %>%
+#'     filter(location=="Mexico" & year==1980 & sex=="Both")
+#' 
+#' aggData <- function(sims, sim_num, m_=m){
+#'     MXDeath %>% select(age_group_id, age_time, age_end) %>%
+#'         mutate(ldeaths=sapply(age_end, function(a) sum(sims < a))) %>%
+#'         mutate(deaths=ldeaths-lag(ldeaths)) %>%
+#'         mutate(deaths=ifelse(is.na(deaths), ldeaths, deaths)) %>%
+#'         mutate(pop_size=m_-lag(ldeaths)) %>%
+#'         mutate(pop_size=ifelse(is.na(pop_size), m_, pop_size)) %>%
+#'         mutate(px=deaths/pop_size, qx=1-px) %>%
+#'         mutate(hx=1-(qx^(1/age_time))) %>% 
+#'         mutate(Sx=cumprod(qx), Fx=1-Sx) %>%
+#'         mutate(simulation=sim_num)
+#' }
+#' 
+#' simDF <- bind_rows(lapply(1:100, function(i) aggData(simDeaths[[i]], i))) 
+#' 
+#' simDF %>% filter(age_end < 115 & hx != 0) %>%
+#'     ggplot(aes(x=age_end, y=hx, color=simulation, group=simulation)) + 
+#'     geom_line(alpha=.3) + 
+#'     geom_line(aes(x=age_end, y=hx, group=1), data=MXDeath, color="red") + 
+#'     coord_trans(y="log") + 
+#'     labs("Simulated Instantaneous Hazard")
+#' 
+#' 
+#' @export
+#' 
+
 GenPopFuncs <- 
     function(location_="United States", year_=2016, sex_="Both", max_age=140){
     library(splines)
